fec2cc5d3c182c50cf5651da15234befb3ef4bc6
Михаил Рояк <mikeroyak@gmail.com>
2025-02-19 13:09:44 +0700

    примеры рисования

--- veceditor/MVVM/View/MainWindow.axaml.cs
+++ veceditor/MVVM/View/MainWindow.axaml.cs
@@ -1,8 +1,11 @@
-using System;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
 using DynamicData;
 using DynamicData.Alias;
 using Paint2.ViewModels;
+using SkiaSharp;
+using System;
 using System.Collections.ObjectModel;
 
 namespace veceditor
@@ -18,6 +21,39 @@ namespace veceditor
          InitializeComponent();
          vm.Figures.Connect().Select(f => f.Name).SortAndBind(out figures);
          vm.Figures.CountChanged.Subscribe(c => { });
+         Draw();
+      }
+      void Draw()
+      {
+         Pen shapeOutlinePen = new Pen(Brushes.Black, 2);
+
+         // Create a DrawingGroup
+         DrawingGroup dGroup = new DrawingGroup();
+
+         // Obtain a DrawingContext from 
+         // the DrawingGroup.
+         using (DrawingContext dc = dGroup.Open())
+         {
+            // Draw a rectangle at full opacity.
+            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(0, 0, 25, 25));
+
+            // This rectangle is drawn at 50% opacity.
+            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(25, 25, 25, 25));
+
+            // This rectangle is blurred and drawn at 50% opacity (0.5 x 0.5). 
+            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(50, 50, 25, 25));
+
+            // This rectangle is also blurred and drawn at 50% opacity.
+            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(75, 75, 25, 25));
+
+
+            // This rectangle is drawn at 50% opacity with no blur effect.
+            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(100, 100, 25, 25));
+            //                dc.DrawGeometry(Brushes.Red, shapeOutlinePen,new PathGeometry();
+            //https://learn.microsoft.com/en-us/dotnet/desktop/wpf/graphics-multimedia/path-markup-syntax?view=netframeworkdesktop-4.8
+         }
+         DrawingImage drawing = new(dGroup);
+         Picture.Source = drawing;
       }
    }
 }
