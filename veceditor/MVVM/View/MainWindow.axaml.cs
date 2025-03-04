a144b963f2836e441d149910c2ac505a5e043642
JackRaycoon <jack.o.raycoon@gmail.com>
2025-03-04 13:14:30 +0700

    Рисование Линий и Кругов
    Временно переключаем только через код, меняем переменную mode

--- veceditor/MVVM/View/MainWindow.axaml.cs
+++ veceditor/MVVM/View/MainWindow.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls;
 using Avalonia.Controls.Shapes;
 using Avalonia.Input;
 using Avalonia.Media;
+using System;
 using System.Collections.Generic;
 
 namespace veceditor
@@ -11,6 +12,13 @@ namespace veceditor
    {
       private Canvas? _canvas;
       private List<Point> _points = new();
+
+      /*
+       * Режим 0 - рисование только точек
+       * Режим 1 - рисование линий
+       * Режим 2 - рисование круга
+      */
+      private int mode = 2;
 
       public MainWindow()
       {
@@ -40,6 +48,41 @@ namespace veceditor
 
          // Добавляем точку на Canvas
          _canvas.Children.Add(ellipse);
+
+         //Отрисовка линии (чисто тестово пока)
+         if(_points.Count % 2 == 0 && mode == 1)
+         {
+            var lineGeom = new LineGeometry
+            {
+               StartPoint = _points[^2], //Второй с конца
+               EndPoint = _points[^1] //Последний
+            };
+            var lineShape = new Path
+            {
+               Stroke = Brushes.Black,
+               StrokeThickness = 2,
+               Data = lineGeom
+            };
+            _canvas.Children.Add(lineShape);
+         }
+         else if (mode == 2 && _points.Count % 2 == 0)
+         {
+            var center = _points[^2];
+            var radiusPoint = _points[^1];
+            var radius = Math.Sqrt(Math.Pow(radiusPoint.X - center.X, 2) + Math.Pow(radiusPoint.Y - center.Y, 2));
+
+            var circle = new Ellipse
+            {
+               Width = radius * 2,
+               Height = radius * 2,
+               Stroke = Brushes.Black,
+               StrokeThickness = 2
+            };
+            Canvas.SetLeft(circle, center.X - radius);
+            Canvas.SetTop(circle, center.Y - radius);
+            _canvas.Children.Add(circle);
+         }
+
       }
    }
 }
