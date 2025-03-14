22f987357991b98184369d0f5fb1cb5794080ab5
3Ilya3 <magistrgoblin@mail.ru>
2025-03-15 00:35:32 +0700

    Добавил геометрую, не закончено

--- veceditor/MVVM/Model/Interfaces.cs
+++ veceditor/MVVM/Model/Interfaces.cs
@@ -1,46 +1,123 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Avalonia.Media;
 
 namespace veceditor.MVVM.Model
 {
-    public class Point
-    {
-        public double x, y;
-    }
-    public interface IFigureGraphicProperties
-    {
-        Color SolidColor { get; }
-        Color BorderColor { get; }
+   public class Point
+   {
+      public double x, y;
 
-    }
-    public interface IGraphicInterface
-    {
-        void DrawCircle(Point Center, double rad);
-    }
-    public interface IDrawableFigure
-    {
-        void Draw(IGraphicInterface graphic);
-    }
+      // Вычисляет расстояние между текущей точкой и другой точкой
+      public double DistanceTo(Point other)
+      {
+         double dx = other.x - x;
+         double dy = other.y - y;
+         return Math.Sqrt(dx * dx + dy * dy);
+      }
 
-    public interface IFigure
-    {
-        bool IsInternal(Point p, double eps);
-        IFigure Intersect(IFigure other);
-        IFigure Union(IFigure other);
-        IFigure Subtract(IFigure other);
-        void Move(Point vector);
-        void Rotate(Point Center, double angle);
-        void Scale(double x, double y);
-        void Scale(Point Center, double rad);
-        void Reflection(Point ax1, Point ax2);
-        IEnumerable<IDrawableFigure> GetDrawFigures();
-        bool IsClosed { get; }
+      // Перемещение точки на заданные расстояния по осям
+      public void Translate(double dx, double dy)
+      {
+         x += dx;
+         y += dy;
+      }
+
+      // Проверка на равенство точек
+      public override bool Equals(object obj)
+      {
+         if (obj is Point other)
+         {
+            return Math.Abs(x - other.x) < double.Epsilon && Math.Abs(y - other.y) < double.Epsilon;
+         }
+         return false;
+      }
+   }
+   public interface IFigureGraphicProperties
+   {
+      // Цвет заливки фигуры
+      Color SolidColor { get; }
+
+      // Цвет границы
+      Color BorderColor { get; }
+
+      // Толщина границы
+      float BorderWidth { get; }
+
+      // Стиль границы
+      DashStyle BorderDashStyle { get; }
+   }
+   public class FigureGraphicProperties : IFigureGraphicProperties
+   {
+      public Color SolidColor { get; set; }
+      public Color BorderColor { get; set; }
+      public float BorderWidth { get; set; }
+      public DashStyle BorderDashStyle { get; set; }
+
+      public FigureGraphicProperties(Color solidColor, Color borderColor, float borderWidth, DashStyle borderDashStyle)
+      {
+         SolidColor = solidColor;
+         BorderColor = borderColor;
+         BorderWidth = borderWidth;
+         BorderDashStyle = borderDashStyle;
+      }
+   }
+   public interface IGraphicInterface
+   {
+      void DrawCircle(Point center, double radius);
+      void DrawLine(Point start, Point end);
+      void DrawRectangle(Point topLeft, double width, double height);
+      void DrawPolygon(IEnumerable<Point> points);
+   }
+   public interface IDrawableFigure
+   {
+      void Draw(IGraphicInterface graphic);
+   }
+   public interface IFigure
+   {
+      bool IsInternal(Point p, double eps);
+      IFigure Intersect(IFigure other);
+      IFigure Union(IFigure other);
+      IFigure Subtract(IFigure other);
+      void Move(Point vector);
+      void Rotate(Point Center, double angle);
+      void Scale(double x, double y);
+      void Scale(Point Center, double rad);
+      void Reflection(Point ax1, Point ax2);
+      IEnumerable<IDrawableFigure> GetDrawFigures();
+      bool IsClosed { get; }
       string Name { get; set; }
    }
+   public abstract class BaseFigure : IFigure, IDrawableFigure
+   {
+      protected IFigureGraphicProperties graphics;
+      public string Name { get; set; }
 
+      public BaseFigure(string name, IFigureGraphicProperties graphics)
+      {
+         Name = name;
+         this.graphics = graphics;
+      }
+
+      public abstract bool IsInternal(Point p, double eps);
+      public abstract IFigure Intersect(IFigure other);
+      public abstract IFigure Union(IFigure other);
+      public abstract IFigure Subtract(IFigure other);
+      public abstract void Move(Point vector);
+      public abstract void Rotate(Point center, double angle);
+      public abstract void Scale(double x, double y);
+      public abstract void Scale(Point center, double rad);
+      public abstract void Reflection(Point ax1, Point ax2);
+      public abstract bool IsClosed { get; }
+      public abstract void Draw(IGraphicInterface graphic);
+
+      public virtual IEnumerable<IDrawableFigure> GetDrawFigures()
+      {
+         return new List<IDrawableFigure> { this };
+      }
+   }
 }
--- veceditor/MVVM/View/MainWindow.axaml.cs
+++ veceditor/MVVM/View/MainWindow.axaml.cs
@@ -3,69 +3,107 @@ using Avalonia.Controls;
 using Avalonia.Media;
 using DynamicData;
 using DynamicData.Alias;
-using SkiaSharp;
 using System;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using vecedidor.MVVM.ViewModel;
+using veceditor.MVVM.Model;
+using MyPoint = veceditor.MVVM.Model.Point; // Псевдоним для Point из interfaces
 
 namespace veceditor
 {
    public partial class MainWindow : Window
    {
-
-      ViewModelBase vm = new ();
+      ViewModelBase vm = new();
       ReadOnlyObservableCollection<string> figures;
-      public ReadOnlyObservableCollection<string> Figures => Figures;
+      public ReadOnlyObservableCollection<string> Figures => figures;
+
       public MainWindow()
       {
          InitializeComponent();
+
+         // Инициализация ViewModel и привязка данных
          vm.Figures.Connect().Select(f => f.Name).SortAndBind(out figures);
          vm.Figures.CountChanged.Subscribe(c => { });
-            vm.FileQuestion.RegisterHandler(async context =>
-            {
-                var picker = await this.StorageProvider.OpenFilePickerAsync(new()
-                {
-                    Title = context.Input,
-                    AllowMultiple = false
-                });
-                var file = picker.FirstOrDefault();
-                if (file != null)
-                    context.SetOutput(file.Path.ToString());
-            });
+
+         // Вызов метода отрисовки
          Draw();
       }
+
       void Draw()
       {
-         Pen shapeOutlinePen = new Pen(Brushes.Black, 2);
-
-         // Create a DrawingGroup
          DrawingGroup dGroup = new DrawingGroup();
 
-         // Obtain a DrawingContext from 
-         // the DrawingGroup.
+
          using (DrawingContext dc = dGroup.Open())
          {
-            // Draw a rectangle at full opacity.
-            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(0, 0, 25, 25));
 
-            // This rectangle is drawn at 50% opacity.
-            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(25, 25, 25, 25));
-
-            // This rectangle is blurred and drawn at 50% opacity (0.5 x 0.5). 
-            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(50, 50, 25, 25));
-
-            // This rectangle is also blurred and drawn at 50% opacity.
-            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(75, 75, 25, 25));
+            var graphicInterface = new AvaloniaGraphicInterface(dc);
 
 
-            // This rectangle is drawn at 50% opacity with no blur effect.
-            dc.DrawRectangle(Brushes.Blue, shapeOutlinePen, new Rect(100, 100, 25, 25));
-            //                dc.DrawGeometry(Brushes.Red, shapeOutlinePen,new PathGeometry();
-            //https://learn.microsoft.com/en-us/dotnet/desktop/wpf/graphics-multimedia/path-markup-syntax?view=netframeworkdesktop-4.8
+            var circle = new Circle(
+                name: "My Circle",
+                graphics: new FigureGraphicProperties(
+                    solidColor: Colors.Blue,
+                    borderColor: Colors.Black,
+                    borderWidth: 2.0f,
+                    borderDashStyle: new DashStyle(new double[] { 2, 2 }, 0) // Пунктирная линия
+                ),
+                center: new MyPoint { x = 100, y = 100 },
+                radius: 50
+            );
+
+
+            circle.Draw(graphicInterface);
          }
+
+
          DrawingImage drawing = new(dGroup);
          Picture.Source = drawing;
+      }
+   }
+
+   // Реализация IGraphicInterface для Avalonia
+   public class AvaloniaGraphicInterface : IGraphicInterface
+   {
+      private readonly DrawingContext _context;
+      private Color _fillColor = Colors.Blue;
+      private Color _borderColor = Colors.Black;
+      private float _borderWidth = 2.0f;
+      private DashStyle _borderDashStyle = new DashStyle(new double[] { 2, 2 }, 0);
+
+      public AvaloniaGraphicInterface(DrawingContext context)
+      {
+         _context = context;
+      }
+
+      public void DrawCircle(MyPoint center, double radius)
+      {
+         var brush = new SolidColorBrush(_fillColor);
+         var pen = new Pen(new SolidColorBrush(_borderColor), _borderWidth, dashStyle: _borderDashStyle);
+         _context.DrawEllipse(brush, pen, new Avalonia.Point(center.x, center.y), radius, radius);
+      }
+
+      public void DrawLine(MyPoint start, MyPoint end)
+      {
+         var pen = new Pen(new SolidColorBrush(_borderColor), _borderWidth, dashStyle: _borderDashStyle);
+         _context.DrawLine(pen, new Avalonia.Point(start.x, start.y), new Avalonia.Point(end.x, end.y));
+      }
+
+      public void DrawRectangle(MyPoint topLeft, double width, double height)
+      {
+         var brush = new SolidColorBrush(_fillColor);
+         var pen = new Pen(new SolidColorBrush(_borderColor), _borderWidth, dashStyle: _borderDashStyle);
+         _context.DrawRectangle(brush, pen, new Rect(topLeft.x, topLeft.y, width, height));
+      }
+
+      public void DrawPolygon(IEnumerable<MyPoint> points)
+      {
+         var brush = new SolidColorBrush(_fillColor);
+         var pen = new Pen(new SolidColorBrush(_borderColor), _borderWidth, dashStyle: _borderDashStyle);
+         var avaloniaPoints = points.Select(p => new Avalonia.Point(p.x, p.y)).ToList();
+         _context.DrawGeometry(brush, pen, new PolylineGeometry(avaloniaPoints, true));
       }
    }
 }
