713aca3eac903b214f70e3f31979a68338464392
JackRaycoon <jack.o.raycoon@gmail.com>
2025-03-17 14:43:10 +0700

    Добавлены новые панельки и кнопки
    Пока панелька с настройками линии чисто есть, её значения ещё ни на что не влияют

---
+++ veceditor/MVVM/View/LineView.axaml.cs
@@ -0,0 +1,15 @@
+﻿using Avalonia.Controls;
+using ReactiveUI;
+using veceditor.MVVM.ViewModel;
+
+namespace veceditor.MVVM.View
+{
+   public partial class LineView : UserControl
+   {
+      public LineView()
+      {
+         InitializeComponent();
+         DataContext = new LineViewModel(); // Привязываем ViewModel
+      }
+   }
+}
